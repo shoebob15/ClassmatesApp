9b0df4255f73277e747358540c8101fb68077ee1
Brennan Reinhard <brennan.reinhard27@d155.org>
2024-10-02 07:27:17 -0500

    Initial Commit

---
+++ ClassmatesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassmatesApp
+//
+//  Created by BRENNAN REINHARD on 10/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
